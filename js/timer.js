fc8f296de3c124d97c32c573619e0d237645c77c
Mila <gil.lyuda@mail.ru>
2023-02-03 21:14:21 +0300

    finish

--- js/timer.js
+++ js/timer.js
@@ -13,15 +13,27 @@ export const showTime = (seconds) => {
     secondsElem.textContent = addZero(seconds % 60);
 };
 
+const title = document.title;
+
 export const startTimer = () => {
-    
+const countdown = new Date().getTime() + state.timeLeft * 1000;
+
     state.timerId = setInterval(() => {
+        
         state.timeLeft--;
         showTime(state.timeLeft);
+
+        document.title = state.timeLeft;
+
+        if (!(state.timeLeft % 5)) {
+            const now = new Date().getTime();
+            state.timeLeft = Math.floor((countdown - now) / 1000);
+        }
 
         if (state.timeLeft > 0 && state.isActive) {
     return;
 }
+
     if (state.status === 'work'){
         state.activeTodo.pomodoro += 1;
         updateTodo(state.activeTodo);
